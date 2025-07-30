94c936f034720195495a8366afab64d9a0680add
kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-07-30 15:43:26 -0400

    feat: add the ability to draw lines and change project settings

---
+++ scripts/main.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@export var line_color: Color
+
+
+func _process(_delta: float) -> void:
+	for child: Node in get_children():
+		if child is Line2D and not child.is_in_group("complete_line"):
+			child.set_point_position(1, get_global_mouse_position())
+
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("left_click"):
+		var line: Line2D = Line2D.new()
+
+		line.default_color = line_color
+
+		line.joint_mode = Line2D.LINE_JOINT_ROUND
+		line.begin_cap_mode = Line2D.LINE_CAP_ROUND
+		line.end_cap_mode = Line2D.LINE_CAP_ROUND
+
+		line.add_point(get_global_mouse_position())
+		line.add_point(get_global_mouse_position())
+
+		add_child(line)
+
+	if Input.is_action_just_released("left_click"):
+		for child: Node in get_children():
+			if child is Line2D and not child.is_in_group("complete_line"):
+				child.add_to_group("complete_line")
+
+				# Add collision to the line
+				for i: int in child.points.size() - 1:
+					var static_body: StaticBody2D = StaticBody2D.new()
+					var collision_shape: CollisionShape2D = CollisionShape2D.new()
+
+					child.add_child(static_body)
+					static_body.add_child(collision_shape)
+
+					var segment_shape: SegmentShape2D = SegmentShape2D.new()
+					segment_shape.a = child.points[i]
+					segment_shape.b = child.points[i + 1]
+
+					collision_shape.shape = segment_shape
